079a447fa542479d0d8accb7bb842e0d78909b7d
User <User@DESKTOP-N7D1EHF>
2019-05-19 10:46:27 +0800

    one

--- src/main/java/com/hxzy/Test.java
+++ src/main/java/com/hxzy/Test.java
@@ -3,7 +3,8 @@ package com.hxzy;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("tom");
+		System.out.println("mike");
 
 	}
 
